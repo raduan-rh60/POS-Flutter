8dc4898f2eacf68a1248372703cc423a18d02d9f
Raduanul Haque <raduanulhaque01@gmail.com>
2025-01-08 13:15:52 +0600

    Pos Application upload

---
+++ pos_system/lib/Login/login.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height / 7, horizontal: 25),
+        color: Colors.white,
+        child: Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/logo.png"),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    floatingLabelStyle:
+                        TextStyle(color: Colors.deepPurpleAccent),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.deepPurpleAccent, width: 3),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    label: Text("User Name"),
+                    suffixIcon: Icon(
+                      Icons.person,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.black)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    floatingLabelStyle:
+                        TextStyle(color: Colors.deepPurpleAccent),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.deepPurpleAccent, width: 3),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    label: Text("User Password"),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye_outlined,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: Colors.black)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.deepPurpleAccent,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  )),
+              SizedBox(
+                height: 10,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
